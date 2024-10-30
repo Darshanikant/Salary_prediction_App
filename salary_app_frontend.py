e42255df1b5a77031dbb88713bc785fd6a082ab5
Darshanikanta Behera <140230452+Darshanikant@users.noreply.github.com>
2024-10-30 20:27:16 +0530

    Add files via upload

---
+++ salary_app_frontend.py
@@ -0,0 +1,28 @@
+
+# import library
+import streamlit as st
+import numpy as np
+import pickle
+
+# load the pickel file to model
+model=pickle.load(open(r"C:\Users\sunil\Desktop\DK\vs code\SALARY_PREDICTION\salary price.pkl","rb"))
+
+# write a titel
+st.title("SALARY PREDICTION App")
+
+#write some text
+st.write("This app provide salary prediction based on your experience .. please enter your year of experience to get the salary prediction..")
+
+# take input from user salary
+
+year=st.number_input("Enter your Experience here..",min_value=0.0, max_value=40.00,value=0.0,step=1.0)
+
+#creat a button
+if st.button("Predict Salary"):
+    #predict
+    exp_input=np.array([[year]]) # creat array
+    prediction=model.predict(exp_input) # predict
+    
+    #return display
+    st.success(f"The pridicted ssalary of {year} is: ${prediction}.")
+    st.write("Thanks for using the model...Hope you got the output..")
---
+++ salary_code_backend.py
@@ -0,0 +1,76 @@
+
+# Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import data set
+data=pd.read_csv(r"C:\Users\sunil\Desktop\DK\vs code\SALARY_PREDICTION\Salary_Data.csv")
+
+
+#split into dependent and independent variable
+x=data.iloc[:,0:1].values
+y=data.iloc[:,-1].values
+
+
+# split the data to train test split
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,train_size=0.8,test_size=0.2,random_state=0)
+
+
+# fit LinearRegression model to x_train, y_train
+
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression() 
+regressor.fit(x_train,y_train)
+
+
+#pridict 
+y_pred=regressor.predict(x_test)
+print('The prediction',y_pred)
+
+#comparision
+comp=pd.DataFrame({"actual":y_test,"predict":y_pred})
+print(comp)
+
+
+# plot visuals
+plt.scatter(x_test,y_test,color="red") # plot data points based on test values
+plt.plot(x_train,regressor.predict(x_train),color="blue") # plot regression line with x_train
+plt.xlabel("year of experience")
+plt.ylabel("Salary")
+plt.title("salary VS experience")
+plt.show()
+
+#slope
+m=regressor.coef_
+print("solpe: ",m)
+
+# intercept/slope
+c=regressor.intercept_
+print("intercept: ",c)
+
+
+# future prediction y=mx + c of 13 year exp
+xp=20
+fpred=m*xp+c 
+print(f"the salary of {xp} year is {fpred}")
+
+# baias and variance score
+bais=regressor.score(x_train,y_train)
+print("The baise score:-",bais)
+
+variance=regressor.score(x_test,y_test)
+print("The variance score :-",variance)
+
+
+# Save the trained model to disk
+import pickle
+filename = 'salary price.pkl'
+with open(filename, 'wb') as file:
+    pickle.dump(regressor, file)
+print("Model has been pickled and saved as salary price.pkl")
+
+import os
+print(os.getcwd())
