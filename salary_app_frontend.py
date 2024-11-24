ce89c926990e65f2d09afce980d9ee9fe37cfe54
Darshanikanta Behera <140230452+Darshanikant@users.noreply.github.com>
2024-11-24 12:26:53 +0530

    Update salary_app_frontend.py

--- salary_app_frontend.py
+++ salary_app_frontend.py
@@ -5,7 +5,7 @@ import numpy as np
 import pickle
 
 # load the pickel file to model
-model=pickle.load(open(r"salary price.pkl","rb"))
+model=pickle.load(open("salary price.pkl","rb"))
 
 # write a titel
 st.title("SALARY PREDICTION App")
